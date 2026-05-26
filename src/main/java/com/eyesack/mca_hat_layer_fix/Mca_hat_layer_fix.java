a4688a3ce07a63b36452c537ebad354f523cafba
isaac <iengland368@gmail.com>
2026-05-25 21:23:29 -0500

    Updated version

--- src/main/java/com/eyesack/mca_hat_layer_fix/Mca_hat_layer_fix.java
+++ src/main/java/com/eyesack/mca_hat_layer_fix/Mca_hat_layer_fix.java
@@ -5,7 +5,7 @@ import net.neoforged.fml.common.Mod;
 
 @Mod(Mca_hat_layer_fix.MODID)
 public class Mca_hat_layer_fix {
-    public static final String MODID = "mca_hat_fix";
+    public static final String MODID = "mca_hat_layer_fix";
 
     public Mca_hat_layer_fix(IEventBus modEventBus) {
     }
